bee321ce802179171c873ea1476fe8eac96e01d0
itsusinn <itsusinn@foxmail.com>
2021-08-18 23:37:39 +0800

    feat: snake uppercase

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate proc_macro;
 extern crate proc_macro2;
+
 use proc_macro2::Span;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Ident};
@@ -9,11 +10,47 @@ pub fn singleton_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStre
     let ast = parse_macro_input!(input as DeriveInput);
 
     let ident = &ast.ident;
-    let uppercase_indent = Ident::new(&ident.to_string().to_ascii_uppercase(), Span::call_site());
+
+    let uppercase: String = ident
+        .to_string()
+        .char_indices()
+        .flat_map(|(index, c)| {
+            if index != 0 && c.is_uppercase() {
+                let mut v = "_".to_string();
+                v.push(c);
+                v.chars().collect::<Vec<_>>()
+            } else {
+                c.to_uppercase().to_string().chars().collect::<Vec<_>>()
+            }
+        })
+        .collect();
+    let uppercase_indent = Ident::new(&uppercase, Span::call_site());
     let expanded: proc_macro2::TokenStream = quote! {
         pub static #uppercase_indent: once_cell::sync::Lazy<#ident> = once_cell::sync::Lazy::new(|| {
             #ident::default()
         });
    }.into();
-   proc_macro::TokenStream::from(expanded)
+    proc_macro::TokenStream::from(expanded)
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        let ident = "DotTest";
+        let uppercase: String = ident
+            .to_string()
+            .char_indices()
+            .flat_map(|(index, c)| {
+                if index != 0 && c.is_uppercase() {
+                    let mut v = "_".to_string();
+                    v.push(c);
+                    v.chars().collect::<Vec<_>>()
+                } else {
+                    c.to_uppercase().to_string().chars().collect::<Vec<_>>()
+                }
+            })
+            .collect();
+        assert_eq!("DOT_TEST", uppercase);
+    }
 }
